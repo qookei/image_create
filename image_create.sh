1ffd78d2084d26486f901e67c91677546125b814
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2019-03-09 11:28:57 +0100

    Dynamically allocate loopback devices

--- image_create.sh
+++ image_create.sh
@@ -43,19 +43,19 @@ gpt_type="EBD0A0A2-B9E5-4433-87C0-68B6B72699C7"
 rm -f $1
 fallocate -l $2 $1
 
-sudo losetup /dev/loop0 $1
+lodev=$(sudo losetup -f --show $1)
 
 case "$4" in
 	"dos" )
 		# For DOS layouts, install GRUB after the MBR.
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel /dev/loop0
+		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
 label: dos
 16MiB + $dos_parttype
 END_SFDISK
 		;;
 	"gpt" )
 		# For GPT layouts, install GRUB to its own partition.
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel /dev/loop0
+		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
 label: gpt
 - 16MiB 21686148-6449-6E6F-744E-656564454649
 - +     $gpt_type
@@ -66,21 +66,21 @@ END_SFDISK
 		exit -2;;
 esac
 
-sudo losetup -d /dev/loop0
-sudo losetup -P /dev/loop0 $1
+sudo losetup -d $lodev
+lodev=$(sudo losetup -Pf --show $1)
 
 # Format root partition according to user-chosen type.
 case "$3" in
 	"fat16" )
-		sudo mkfs.vfat -F 16 /dev/loop0$rootpart;;
+		sudo mkfs.vfat -F 16 $lodev$rootpart;;
 	"fat32" )
-		sudo mkfs.vfat -F 32 /dev/loop0$rootpart;;
+		sudo mkfs.vfat -F 32 $lodev$rootpart;;
 	"ext2" )
-		sudo mkfs.ext2 /dev/loop0$rootpart;;
+		sudo mkfs.ext2 $lodev$rootpart;;
 	"ext3" )
-		sudo mkfs.ext3 /dev/loop0$rootpart;;
+		sudo mkfs.ext3 $lodev$rootpart;;
 	"ext4" )
-		sudo mkfs.ext4 /dev/loop0$rootpart;;
+		sudo mkfs.ext4 $lodev$rootpart;;
 	* )
 		echo "unsupported partition type, how did we get here?";
 		exit -3;;
@@ -90,15 +90,15 @@ mountpoint=$(mktemp -d)
 
 echo "tmp mountpoint is $mountpoint"
 
-sudo mount /dev/loop0$rootpart $mountpoint
+sudo mount $lodev$rootpart $mountpoint
 sudo mkdir -p $mountpoint/boot
-sudo grub-install --target=i386-pc --boot-directory=$mountpoint/boot /dev/loop0
+sudo grub-install --target=i386-pc --boot-directory=$mountpoint/boot $lodev
 
 if (($# == 5)); then
 	sudo cp -avr $5/* $mountpoint/
 fi
 
-sudo umount /dev/loop0$rootpart
+sudo umount $lodev$rootpart
 rmdir $mountpoint
 
-sudo losetup -d /dev/loop0
+sudo losetup -d $lodev
