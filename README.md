ebc9b03b0875cde74278c23fde0ce583c19c802e
Kacper Słomiński <kacper.slominski72@gmail.com>
2021-03-14 14:37:24 +0100

    Replace TomatBoot with Limine EFI. Update command line argument parsing.

--- README.md
+++ README.md
@@ -3,29 +3,34 @@
 A simple tool to generate partitioned hard disk images.
 
 ## Requirements
+ - losetup
  - sfdisk
  - mkfs for the corresponding file system
- - losetup
- - grub-install
+   - mkfs.vfat for fat16/32 (note: also required for EFI images, since they need a FAT32 ESP partition)
+   - mkfs.ext2/3/4 for ext2/3/4 respectively
+ - grub-install or git (for cloning limine, can omit cloning by specifying `-n`)
 
 ## Usage
-When you run image\_create without all required arguments, it'll print the following usage message:
+When you run image\_create without any arguments, or with `-h`, it'll print the following usage message:
 ```
-<output file name> <image size> <partition type> <layout> [files]
+usage: image_create.sh [-o output] [-t partition type] [-p partition scheme] [-s size] [-l loader] [-b] [-e] [-n] [-c path]
+
+Supported arguments:
+	 -o output                specifies path to output image
+	 -t partition type        specifies the partition type for the root fs
+	                          supported types: ext2/3/4, fat16/32
+	 -p partition scheme      specifies the partition scheme to use
+	                          supported schemes: mbr, gpt
+	                          note: EFI requires gpt
+	 -s size                  specifies the image size, eg: 1G, 512M
+	 -l loader                specifies the loader to use
+	                          supported loaders: grub, limine
+	 -b                       makes the image BIOS bootable
+	 -e                       makes the image EFI bootable
+	 -n                       don't fetch Limine (reuses existing './limine/' directory)
+	 -c path                  copy files from the specified directory into the root of the image
+	 -h                       show this help message
+
+When using GPT, you can specify the GUID of the root partition by setting the GPT_TYPE environment variable.
+By default, the GUID for a Windows data partition is used.
 ```
-Here's the explanation of the arguments:
- - `output file name` - file name for the final image file
- - `image size` - size of the image, this option can also take units(16MiB, 1GiB, etc)
- - `partition type` - partiton type, currently supported are `fat16`, `fat32`, `ext2`, `ext3`, `ext4`
- - `layout` - which partition table scheme and bootloader to use
- - `files` - optional argument, path to directory containing files that should be copied to the image
-
-Supported layouts:
- - `dos` - MBR image with GRUB 2 for legacy BIOS
- - `gpt` - GPT image with GRUB 2 for legacy BIOS
- - `x86_64-efi` - GPT image with GRUB 2 for UEFI
- - `x86_64-efi-hybrid` - GPT image with GRUB 2 for both UEFI and legacy BIOS
- - `gpt-limine` - GPT image with limine for legacy BIOS
- - `gpt-tomatboot` - GPT image with TomatBoot for UEFI
- - `gpt-stivale-hybrid` - GPT image with limine for legacy BIOS and TomatBoot for UEFI
-
--- image_create.sh
+++ image_create.sh
@@ -2,191 +2,266 @@
 
 set -e
 
-if (($# != 4 && $# != 5)); then
-	echo "usage: <output file name> <image size> <partition type> <layout> [files]"
-	echo "Supported partition types:"
-	echo -e "\t- fat16"
-	echo -e "\t- fat32"
-	echo -e "\t- ext2"
-	echo -e "\t- ext3"
-	echo -e "\t- ext4\n"
-	echo "Supported layouts:"
-	echo -e "\t- dos - MBR image with GRUB 2 for legacy BIOS"
-	echo -e "\t- gpt - GPT image with GRUB 2 for legacy BIOS"
-	echo -e "\t- x86_64-efi - GPT image with GRUB 2 for UEFI"
-	echo -e "\t- x86_64-efi-hybrid - GPT image with GRUB 2 for both UEFI and legacy BIOS"
-	echo -e "\t- gpt-limine - GPT image with limine for legacy BIOS"
-	echo -e "\t- gpt-tomatboot - GPT image with TomatBoot for UEFI"
-	echo -e "\t- gpt-stivale-hybrid - GPT image with limine for legacy BIOS and TomatBoot for UEFI\n"
-	exit -1
+usage() {
+	echo -e "usage: $0 [-o output] [-t partition type] [-p partition scheme] [-s size] [-l loader] [-b] [-e] [-n] [-c path]\n"
+
+	echo -e "Supported arguments:"
+	echo -e "\t -o output                specifies path to output image"
+	echo -e "\t -t partition type        specifies the partition type for the root fs"
+	echo -e "\t                          supported types: ext2/3/4, fat16/32"
+	echo -e "\t -p partition scheme      specifies the partition scheme to use"
+	echo -e "\t                          supported schemes: mbr, gpt"
+	echo -e "\t                          note: EFI requires gpt"
+	echo -e "\t -s size                  specifies the image size, eg: 1G, 512M"
+	echo -e "\t -l loader                specifies the loader to use"
+	echo -e "\t                          supported loaders: grub, limine"
+	echo -e "\t -b                       makes the image BIOS bootable"
+	echo -e "\t -e                       makes the image EFI bootable"
+	echo -e "\t -n                       don't fetch Limine (reuses existing './limine/' directory)"
+	echo -e "\t -c path                  copy files from the specified directory into the root of the image"
+	echo -e "\t -h                       shows this help message\n"
+
+	echo "When using GPT, you can specify the GUID of the root partition by setting the GPT_TYPE environment variable."
+	echo "By default, the GUID for a Windows data partition is used."
+
+}
+
+if [ $# -eq 0 ]; then
+	usage
+	exit 0
+fi
+
+output=
+parttype=
+partscheme=
+size=
+loader=
+bios=
+efi=
+no_fetch_limine=
+copy_dir_path=
+
+while getopts o:t:p:s:l:benc:h arg
+do
+	case $arg in
+		o) output="$OPTARG";;
+		t) parttype="$OPTARG"
+			case "$parttype" in
+				ext2|ext3|ext4|fat16|fat32);;
+				*) echo "Partition type $parttype is not supported"; exit 1;;
+			esac
+			;;
+		p) partscheme="$OPTARG"
+			case "$partscheme" in
+				mbr|gpt);;
+				*) echo "Partition scheme $partscheme is not supported"; exit 1;;
+			esac
+			;;
+		s) size="$OPTARG";;
+		l) loader="$OPTARG"
+			case "$loader" in
+				grub|limine);;
+				*) echo "Loader $loader is not supported"; exit 1;;
+			esac
+			;;
+		b) bios=1;;
+		e) efi=1;;
+		n) no_fetch_limine=1;;
+		c) copy_dir_path="$OPTARG";;
+		h) usage; exit 0;;
+		?) echo "See -h for help."; exit 1;;
+	esac
+done
+
+if [ -z "$output" ]; then
+	echo "Output image is required but wasn't specified."
+	echo "See -h for help."
+	exit 1;
+fi
+
+if [ -z "$parttype" ]; then
+	echo "Partition type is required but wasn't specified."
+	echo "See -h for help."
+	exit 1;
+fi
+
+if [ -z "$partscheme" ]; then
+	echo "Partition scheme is required but wasn't specified."
+	echo "See -h for help."
+	exit 1;
+fi
+
+if [ -z "$size" ]; then
+	echo "Size is required but wasn't specified."
+	echo "See -h for help."
+	exit 1;
+fi
+
+if [ -z "$loader" ]; then
+	echo "Loader is required but wasn't specified."
+	echo "See -h for help."
+	exit 1;
+fi
+
+if [ -z "$bios" ] && [ -z "$efi" ]; then
+	echo "Either BIOS or EFI (or both) support needs to be specified but wasn't."
+	echo "See -h for help."
+	exit 1;
+fi
+
+if [ "$efi" ] && [ "$partscheme" != "gpt" ]; then
+	echo "EFI bootable images require GPT partition scheme."
+	exit 1;
+fi
+
+if [ "$loader" = "limine" ]; then
+	if [ -z $no_fetch_limine ]; then
+		rm -rf limine
+	fi
+
+	if [ ! -d limine ]; then
+		git clone https://github.com/limine-bootloader/limine --branch=v2.0-branch-binary --depth=1
+	fi
 fi
 
 dos_parttype=""
-case "$3" in
-	"fat16" )
+case "$parttype" in
+	fat16)
 		dos_parttype="0e";;
-	"fat32" )
+	fat32)
 		dos_parttype="0c";;
-	"ext2" )
+	ext2)
 		dos_parttype="83";;
-	"ext3" )
+	ext3)
 		dos_parttype="83";;
-	"ext4" )
+	ext4)
 		dos_parttype="83";;
-	* )
-		echo "unsupported partition type";
-		exit -2;;
 esac
 
 rootpart=""
-case "$4" in
-	"dos" | "gpt-limine" )
+case "$partscheme" in
+	mbr)
 		rootpart="p1"
 		;;
-	"gpt" | "x86_64-efi" | "gpt-tomatboot" | "gpt-stivale-hybrid" )
-		rootpart="p2"
+	gpt)
+		if [ "$loader" = "grub" ] && [ "$bios" ] && [ "$efi" ]; then
+			rootpart="p3"
+		else
+			rootpart="p2"
+		fi
 		;;
-	"x86_64-efi-hybrid" )
-		rootpart="p3"
-		;;
-	* )
-		echo "unexpected partition table layout";
-		exit -2;;
 esac
 
 # UUID of Windows data partition. Choose something else depending on your needs.
 gpt_type=${GPT_TYPE:="EBD0A0A2-B9E5-4433-87C0-68B6B72699C7"}
 
-rm -f $1
-fallocate -l $2 $1
+rm -f "$output"
+fallocate -l "$size" "$output"
 
-lodev=$(sudo losetup -f --show $1)
+lodev=$(sudo losetup -f --show "$output")
 
-case "$4" in
-	"dos" )
-		# For DOS layouts, install GRUB's boot code after the MBR.
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
+case "$partscheme" in
+	dos)
+		# For MBR layouts, reserve some space for the loader after the MBR.
+		cat << END_SFDISK | sudo sfdisk --no-tell-kernel "$lodev"
 label: dos
-16MiB + $dos_parttype
+- 16MiB
+- +     $dos_parttype
 END_SFDISK
 		;;
-	"gpt" )
-		# For GPT layouts, install GRUB's boot code to a "BIOS boot partition".
-		# GRUB will use the entire partition in this case.
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
+	gpt)
+		if [ -z "$efi" ] && [ "$loader" = "grub" ]; then
+			# Create a BIOS boot partition for GRUB's boot code.
+			# GRUB will use the entire partition in this case.
+			cat << END_SFDISK | sudo sfdisk --no-tell-kernel "$lodev"
 label: gpt
 - 16MiB 21686148-6449-6E6F-744E-656564454649
 - +     $gpt_type
 END_SFDISK
-		;;
-	"gpt-limine" )
-		# For GPT layouts, limine's boot code is embedded in GPT structures.
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
+		elif [ -z "$efi" ] && [ "$loader" = "limine" ]; then
+			# Limine's boot code is embedded in GPT structures.
+			cat << END_SFDISK | sudo sfdisk --no-tell-kernel "$lodev"
 label: gpt
 - +     $gpt_type
 END_SFDISK
-		;;
-	"x86_64-efi" | "gpt-tomatboot" | "gpt-stivale-hybrid" )
-		# For GPT layouts, install GRUB's/tomatboot's boot code to the EFI system partition.
-		# GRUB will create a file on that partition.
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
+		elif [ -z "$bios" ] || [ "$loader" = "limine" ]; then
+			# Create an EFI system partition for GRUB's/Limine's boot files.
+			cat << END_SFDISK | sudo sfdisk --no-tell-kernel "$lodev"
 label: gpt
-- 512MiB C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+- 16MiB C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 - +      $gpt_type
 END_SFDISK
-		;;
-	"x86_64-efi-hybrid" )
-		# Combined GRUB EFI + GRUB legacy layout
-		cat << END_SFDISK | sudo sfdisk --no-tell-kernel $lodev
+		else
+			# Combined GRUB EFI + GRUB legacy layout.
+			cat << END_SFDISK | sudo sfdisk --no-tell-kernel "$lodev"
 label: gpt
 - 16MiB C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 - 16MiB 21686148-6449-6E6F-744E-656564454649
 - +     $gpt_type
 END_SFDISK
+		fi
 		;;
-	* )
-		echo "unexpected partition table layout, how did we get here?";
-		exit -2;;
 esac
 
-sudo losetup -d $lodev
-lodev=$(sudo losetup -Pf --show $1)
+sudo losetup -d "$lodev"
+lodev=$(sudo losetup -Pf --show "$output")
 
 # Format root partition according to user-chosen type.
-case "$3" in
-	"fat16" )
-		sudo mkfs.vfat -F 16 $lodev$rootpart;;
-	"fat32" )
-		sudo mkfs.vfat -F 32 $lodev$rootpart;;
-	"ext2" )
-		sudo mkfs.ext2 $lodev$rootpart;;
-	"ext3" )
-		sudo mkfs.ext3 $lodev$rootpart;;
-	"ext4" )
-		sudo mkfs.ext4 $lodev$rootpart;;
-	* )
-		echo "unsupported partition type, how did we get here?";
-		exit -3;;
+case "$parttype" in
+	fat16)
+		sudo mkfs.vfat -F 16 "$lodev$rootpart";;
+	fat32)
+		sudo mkfs.vfat -F 32 "$lodev$rootpart";;
+	ext2)
+		sudo mkfs.ext2 "$lodev$rootpart";;
+	ext3)
+		sudo mkfs.ext3 "$lodev$rootpart";;
+	ext4)
+		sudo mkfs.ext4 "$lodev$rootpart";;
 esac
 
 mountpoint=$(mktemp -d)
-echo "tmp mountpoint is $mountpoint"
-sudo mount $lodev$rootpart $mountpoint
+echo "Mountpoint is $mountpoint"
+sudo mount "$lodev$rootpart" "$mountpoint"
 
-sudo mkdir $mountpoint/boot
+sudo mkdir "$mountpoint/boot"
 
-case "$4" in
-	"dos" | "gpt" | "x86_64-efi-hybrid" )
-		# i386-pc makes GRUB use the MBR or BIOS boot partition for its boot code,
-		# depending on the partition table type.
-		# Note that we do not have to partition the BIOS boot partition.
-		sudo grub-install --target=i386-pc --boot-directory=$mountpoint/boot $lodev
-		;;&
-	"gpt-limine" | "gpt-stivale-hybrid" )
-		# Use installed limine-install if available
-		LIMINE_INSTALL="limine-install"
-		if ! command -v "$LIMINE_INSTALL" &> /dev/null
-		then
-			if [ -d limine ]; then
-				rm -rf limine
-			fi
-
-			git clone https://github.com/limine-bootloader/limine.git --branch=v1.0-branch --depth=1
-			make -C limine limine-install
-
-			LIMINE_INSTALL="./limine/limine-install"
-		fi
-		sudo "$LIMINE_INSTALL" ${lodev}
-		;;&
-	"gpt-tomatboot" | "gpt-stivale-hybrid" )
-		# EFI installations require the EFI system partition to be mounted.
-		if ! [ -f tomatboot.efi ];
-		then
-			wget https://raw.githubusercontent.com/qookei/image_create/master/tomatboot.efi
-		fi
-
-		sudo mkfs.vfat ${lodev}p1
-		sudo mkdir $mountpoint/boot/efi
-		sudo mount ${lodev}p1 $mountpoint/boot/efi
-		sudo mkdir -p $mountpoint/boot/efi/efi/boot
-		sudo cp tomatboot.efi $mountpoint/boot/efi/efi/boot/BOOTX64.EFI
-		sudo umount ${lodev}p1
-		;;
-	"x86_64-efi" | "x86_64-efi-hybrid" )
-		# EFI installations require the EFI system partition to be mounted.
-		sudo mkfs.vfat ${lodev}p1
-		sudo mkdir $mountpoint/boot/efi
-		sudo mount ${lodev}p1 $mountpoint/boot/efi
-		sudo grub-install --target=x86_64-efi --removable --boot-directory=$mountpoint/boot --no-uefi-secure-boot $lodev
-		sudo umount ${lodev}p1
-		;;
-esac
-
-if (($# == 5)); then
-	sudo cp -avr $5/* $mountpoint/
+if [ "$bios" ]; then
+	case "$loader" in
+		grub)
+			# i386-pc makes GRUB use the MBR or BIOS boot partition for its boot code,
+			# depending on the partition table type.
+			# Note that we do not have to partition the BIOS boot partition.
+			sudo grub-install --target=i386-pc --boot-directory="$mountpoint/boot" "$lodev"
+			;;
+		limine)
+			(cd limine; sudo ./limine-install-linux-x86_64 "$lodev")
+			;;
+	esac
 fi
 
-sudo umount $lodev$rootpart
-rmdir $mountpoint
+if [ "$efi" ]; then
+	sudo mkfs.vfat "${lodev}p1"
+	sudo mkdir "$mountpoint/boot/efi"
+	sudo mount "${lodev}p1" "$mountpoint/boot/efi"
+	sudo mkdir -p "$mountpoint/boot/efi/efi/boot"
 
-sudo losetup -d $lodev
+	case "$loader" in
+		grub)
+			sudo grub-install --target=x86_64-efi --removable --boot-directory="$mountpoint/boot" --no-uefi-secure-boot "$lodev"
+			;;
+		limine)
+			sudo cp limine/BOOTX64.EFI "$mountpoint/boot/efi/efi/boot/BOOTX64.EFI"
+			;;
+	esac
+
+	sudo umount "${lodev}p1"
+fi
+
+if [ "$copy_dir_path" ]; then
+	sudo cp -avr "$copy_dir_path"/* "$mountpoint/"
+fi
+
+sudo umount "$lodev$rootpart"
+rmdir "$mountpoint"
+sudo losetup -d "$lodev"
